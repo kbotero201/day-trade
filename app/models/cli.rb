2fd0928326668c219cf0b26471a47f9e4280eabd
jonathanx111 <jonathanx111@gmail.com>
2020-11-10 01:13:28 -0500

    enabled game instance to update balance based on shares purchased

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -6,6 +6,13 @@ require 'json'
 
 
 class CLI
+    def price=(price)
+        @price=price
+    end
+
+    def price
+        @price
+    end
     @@prompt = TTY::Prompt.new
     @@artii = Artii::Base.new :font => 'slant'
     @@user = nil
@@ -45,9 +52,27 @@ class CLI
     def random_symbol
         stock_symbols= ["GOOG", "IBM", "SPY"]
         stock_symbols.sample
-        price_api_resp = RestClient.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{stock_symbols.sample}&interval=60min&outputsize=full&apikey=VLBVHKO2VWRJNR1W")
+        price_api_resp = RestClient.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{stock_symbols.sample}&interval=60min&outputsize=full&apikey=M0HFRMUEXY3UAOWU")
         price_api_data =JSON.parse(price_api_resp)
         Spi.create(stock_symbol: price_api_data["Meta Data"]["2. Symbol"])
+    end
+
+    def stock_price(stock_name, date, time)
+        price_api_resp = RestClient.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{stock_name}&interval=60min&outputsize=full&apikey=M0HFRMUEXY3UAOWU")
+        price_api_data =JSON.parse(price_api_resp)
+        price_api_data["Time Series (60min)"]["#{date} #{time}"]["1. open"]
+    end
+
+    def rsi(stock_name, date, time)
+       rsi_api_resp = RestClient.get("https://www.alphavantage.co/query?function=RSI&symbol=#{stock_name}&interval=60min&time_period=14&series_type=open&apikey=M0HFRMUEXY3UAOWU")
+       rsi_api_data =JSON.parse(rsi_api_resp)
+       rsi_api_data["Technical Analysis: RSI"]["#{date} #{time}"]["RSI"]
+    end 
+
+    def macd(stock_name, date, time)
+        macd_api_resp = RestClient.get("https://www.alphavantage.co/query?function=MACD&symbol=#{stock_name}&interval=60min&series_type=open&apikey=M0HFRMUEXY3UAOWU")
+        macd_api_data = JSON.parse(macd_api_resp)
+        macd_api_data["Technical Analysis: MACD"]["#{date} #{time}"]["MACD"]
     end
 
 
@@ -61,14 +86,34 @@ class CLI
         case action
             when 1
                 new1 = self.random_symbol
-                 macd_api_resp = RestClient.get("https://www.alphavantage.co/query?function=MACD&symbol=#{new1.stock_symbol}&interval=60min&series_type=open&apikey=VLBVHKO2VWRJNR1W")
+                 macd_api_resp = RestClient.get("https://www.alphavantage.co/query?function=MACD&symbol=#{new1.stock_symbol}&interval=60min&series_type=open&apikey=M0HFRMUEXY3UAOWU")
                 macd_api_data = JSON.parse(macd_api_resp)
-                @@game = Game.new
+                @@game = Game.create
                 @@game.user_balance = 100000
                 @@game.date = macd_api_data["Technical Analysis: MACD"].to_a.sample[0].split(" ")[0]
                 puts "Hi, #{@@user.username}, your balance is #{@@game.user_balance} "
                 puts "For this game, your stock is: #{new1.stock_symbol}"
                 puts "Your startdate is #{@@game.date}"
+                price = self.stock_price(new1.stock_symbol, @@game.date, "09:00:00").to_f
+                puts "The stock price at 9am is #{price}"
+                puts "The RSI is #{self.rsi(new1.stock_symbol, @@game.date, "09:00")}, and the MACD is #{self.macd(new1.stock_symbol, @@game.date, "09:00")}"
+                choices = {"Buy Stocks" => 1,
+                           "Hold and wait to see price at next hour" => 2
+                }
+                action = @@prompt.select("What would you like to do?", choices)
+                case action 
+
+                    when 1
+                        puts "How many shares would you like to buy?"
+                        choice = gets.chomp.to_i
+                        total = choice * price
+                        puts "You've purchsed #{choice} shares at #{price} a share for a total of $#{total}!" 
+                        @@game.user_balance -= total 
+                        puts "Your available funds for trading is now #{@@game.user_balance}!"
+                        
+                        
+
+                end
             
         end
                 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@ Spi.destroy_all
 User.destroy_all
 Game.destroy_all
 
-price_api_resp = RestClient.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&outputsize=full&apikey=VLBVHKO2VWRJNR1W")
+price_api_resp = RestClient.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&outputsize=full&apikey=M0HFRMUEXY3UAOWU")
 price_api_data =JSON.parse(price_api_resp)
 
-rsi_api_resp = RestClient.get("https://www.alphavantage.co/query?function=RSI&symbol=IBM&interval=60min&time_period=14&series_type=open&apikey=VLBVHKO2VWRJNR1W")
+rsi_api_resp = RestClient.get("https://www.alphavantage.co/query?function=RSI&symbol=IBM&interval=60min&time_period=14&series_type=open&apikey=M0HFRMUEXY3UAOWU")
 rsi_api_data =JSON.parse(rsi_api_resp)
 
-macd_api_resp = RestClient.get("https://www.alphavantage.co/query?function=MACD&symbol=IBM&interval=60min&series_type=open&apikey=VLBVHKO2VWRJNR1W")
+macd_api_resp = RestClient.get("https://www.alphavantage.co/query?function=MACD&symbol=IBM&interval=60min&series_type=open&apikey=M0HFRMUEXY3UAOWU")
 macd_api_data = JSON.parse(macd_api_resp)
 
 
 
 
 
-Spi.create(stock_symbol: price_api_data["Meta Data"]["2. Symbol"])
+
 
 User.create(username: "Caryn", password: "12345")
